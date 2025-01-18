1d40dc168c586e5650240d2783ec965c1e6eab99
Patrick Silva <71387333+oliveira-da-silva-patrick@users.noreply.github.com>
2025-01-18 14:29:37 +0100

    update

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     const baseFolder = "pics/";
     try {
         const response = await fetch("images.json");
-        const files = await response.json();
+        const data = await response.json();
 
-        files.forEach(filename => {
+        data.forEach(item => {
+            const { filename, link } = item;
             const [category, ...rest] = filename.split("_");
             const alt = rest.join(" ").replace(/[-_]/g, " ").replace(/\.[^/.]+$/, "");
 
             const targetName = category.concat('-content');
             const categoryDiv = document.getElementById(targetName);
 
-            if (targetName) {
+            if (categoryDiv) {
+                const linkElement = document.createElement("a");
+                linkElement.href = link;
+                linkElement.target = "_blank";
+
                 const imgElement = document.createElement("img");
                 imgElement.src = `${baseFolder}${filename}`;
                 imgElement.alt = alt;
 
-                categoryDiv.appendChild(imgElement);
+                linkElement.appendChild(imgElement);
+                categoryDiv.appendChild(linkElement);
             }
         });
     } catch (error) {
-        console.error("Error loading image data:", error);
+        console.error("Error loading data:", error);
     }
 });
