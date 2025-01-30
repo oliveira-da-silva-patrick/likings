736e90b4537f1107956363ece18989f1ebec07dc
Patrick Silva <71387333+oliveira-da-silva-patrick@users.noreply.github.com>
2025-01-30 12:56:21 +0100

    update

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const baseFolder = "pics/";
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
     try {
         const response = await fetch("images.json");
         let data = await response.json();
 
-        data = shuffleArray(data);
-
         data.forEach(item => {
-            const { filename, link } = item;
-            const [category, ...rest] = filename.split("_");
-            const alt = rest.join(" ").replace(/[-_]/g, " ").replace(/\.[^/.]+$/, "");
+            const { img, link, category, year } = item;
 
             const targetName = category.concat('-content');
             const categoryDiv = document.getElementById(targetName);
@@ -29,8 +15,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 linkElement.target = "_blank";
 
                 const imgElement = document.createElement("img");
-                imgElement.src = `${baseFolder}${filename}`;
-                imgElement.alt = alt;
+                imgElement.src = `${img}`;
 
                 linkElement.appendChild(imgElement);
                 categoryDiv.appendChild(linkElement);
